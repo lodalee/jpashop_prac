b748d55206b55deb1c615c0a68f0ebad552fa03b
chaewon <cwlee4633@naver.com>
2024-07-26 01:42:56 +0900

    feat : add item entity, [Album, Book, Movie] entity
    -Item 패키지 생성
    
    -아이템 엔티티 추가 + 관련 class 추가 및 상속 관계 설정
    
    -싱글테이블 전략
    
    -오더 아이템 엔티티와 아이템 엔티티 테이블 관계 설정

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Item.Item;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,8 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "item_id")
     private Item item;
 
     @ManyToOne

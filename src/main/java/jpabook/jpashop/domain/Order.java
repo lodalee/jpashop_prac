29960b52f0fc46b485f4613c45b2ecf03f47c75d
chaewon <cwlee4633@naver.com>
2024-07-27 19:03:44 +0900

    feat: add relationship methods to Order entity
    - 주문과 회원을 연관시키는 setMember 메서드 추가
    - 주문과 주문 항목을 연관시키는 addOrderItem 메서드 추가
    - 주문과 배송을 연관시키는 setDelivery 메서드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,4 +33,22 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문 상태 [ORDER, CANCEL]
 
+    //==연관관계 메서드==//
+
+    //Member 가 가지고 있는 Orders 목록에 현재 '객체 추가'
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    //일대다 일대일관계에서는 하나의 객체에만 '값을 설정'하는 것
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }

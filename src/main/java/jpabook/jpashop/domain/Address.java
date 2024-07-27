0db699791fbee262cbffed5efc63cc42bcac589f
chaewon <cwlee4633@naver.com>
2024-07-27 18:13:30 +0900

    feat : add category entity,
    - 카테고리 엔티티와 아이템 엔티티 관계 설정
    
    - 클래스명 변경
    Categories -> Category
    
    - 전체 엔티티 지연로딩과 cascade 관계 설정

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,15 @@ public class Address {
     private String street;
     private String zipcode;
 
+    // 값 타입은 변경 불가능하게 설계
+    // JPA 에서는 protected 까지 허용 - 리플렉션 같은 기술 사용 위해서
+    protected Address() {
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }

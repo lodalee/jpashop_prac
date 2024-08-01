8f18336b259fc7faf0d0bbf3fbc36294d1fbeaa0
chaewon <cwlee4633@naver.com>
2024-08-01 23:41:49 +0900

    feat: add business logic to Item entity
    - Item entity 비즈니스 로직 추가
    
    - NotEnoughStockException class 추가

--- src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.Item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.excrption.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,5 +24,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> category;
+
+    //==비즈니스 로직==//
+
+    /**
+     * 재고 수 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 수 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
